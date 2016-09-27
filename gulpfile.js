877920d3afc10d901b0524ade3ba9649948ba546
arodjabel <arodjabel@gmail.com>
2016-09-26 21:14:11 -0500

    youtube, css, text content, various changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,113 @@
 var gulp = require('gulp');
-var server = require('./app.js');
-var app;
+var fs = require('fs');
+var path = require('path');
+var Zip = require('adm-zip');
+var exec = require('exec');
+var filesToCompress,
+    dirsToCompress;
 
-gulp.task('startServer', function () {
-    // Start the server at the beginning of the task 
-    server.startServer();
-});
+var archiver = require('archiver');
+var awsBeanstalk = require("node-aws-beanstalk");
 
-gulp.task('stopServer', function () {
-    // Start the server at the beginning of the task 
-    app.close();
-});
+filesToCompress = ['app.js', 'hemma.js', 'index.html', 'package.json'];
+dirsToCompress = ['images', 'server', 'site'];
+
+function createArchive(dir) {
+    return new Promise(function (resolve, reject) {
+        var archive,
+            output,
+            f,
+            d,
+            thisDir,
+            destPath,
+            finalizeCounter = 0,
+            zipName = './Archive.zip';
+
+        // creates new in memory zip
+
+        var output = fs.createWriteStream(__dirname + '/Archive.zip');
+        var archive = archiver.create('zip', {});
+        archive.pipe(output);
+
+        // addLocalFile(String localPath, String zipPath)
+        // addLocalFolder(String localPath, String zipPath)
+        for (f = 0; f < filesToCompress.length; f++) {
+            // archive.addLocalFile(path.join(__dirname, filesToCompress[f]));
+            archive.file(path.join(__dirname, filesToCompress[f]), {name: filesToCompress[f]})
+            finalizeArchive();
+        }
+
+        for (d = 0; d < dirsToCompress.length; d++) {
+            // archive.addLocalFolder(path.join(__dirname, dirsToCompress[d]), path.join('./', dirsToCompress[d]));
+            archive.directory(path.join(__dirname, dirsToCompress[d]), path.join('./', dirsToCompress[d]), {name: dirsToCompress[d]})
+            finalizeArchive();
+        }
+
+        function finalizeArchive() {
+            finalizeCounter++;
+            if (finalizeCounter === (filesToCompress.length + dirsToCompress.length)) {
+                // archive.writeZip(path.join(__dirname, zipName));
+                archive.finalize();
+                console.log('Archive.zip has been written');
+                resolve();
+            }
+        }
+    })
+}
+
+function deleteArchive(dir, archive) {
+    return new Promise(function (resolve, reject) {
+        fs.unlink(archive, function (err) {
+            if (!err) {
+                console.log('deleted Archive.zip');
+                createArchive(dir).then(function () {
+                    resolve();
+                })
+            } else {
+                console.log('deleting Archive.zip failed');
+                resolve();
+            }
+        })
+    })
+}
+
+deployFn = function () {
+    return new Promise(function (resolve, reject) {
+        var dir,
+            archive;
+
+        dir = path.resolve(__dirname);
+        archive = path.resolve(__dirname, 'Archive.zip');
+
+        //does archive exist?
+        function startDeploy() {
+            return new Promise(function (resolve, reject) {
+                fs.open(archive, 'r', function (err, fd) {
+                    if (err) {
+                        if (err.code === "ENOENT") {
+                            console.error('Archive.zip is not available');
+                            createArchive(dir).then(resolve);
+                        } else {
+                            deleteArchive(dir, archive).then(resolve);
+                        }
+                    } else {
+                        deleteArchive(dir, archive).then(resolve);
+                    }
+                });
+            })
+        }
+
+        startDeploy().then(function () {
+            // exec(['eb deploy', '--staged'], function(err, out, code) {
+            //     if (err instanceof Error){
+            //         throw err;
+            //     }
+            //     process.stderr.write(err);
+            //     process.stdout.write(out);
+            //     process.exit(code);
+            // });
+        });
+    });
+};
+
+gulp.task('deploy', deployFn);
--- site/pages/resources/resources.js
+++ site/pages/resources/resources.js
@@ -6,31 +6,47 @@ angular.module('hemma.resources', [])
         return {
             bindToController: true,
             controllerAs: 'res',
-            controller: [function(){
-                var res = this;
-                res.onlineSources = onlineSources;
-                res.videos = videos;
-            }],
+            controller: ['$scope', '$compile',
+                function($scope, $compile){
+                    var res = this;
+                    res.onlineSources = onlineSources;
+                    res.videos = videos;
+                    res.compile = function(string){
+                        return $compile(angular.element(string))($scope);
+                    }
+                }
+            ],
             templateUrl: './site/pages/resources/resources.html'
+        }
+    }
+])
+.directive('embeddedVideo', ['$compile', '$timeout',
+    function($compile, $timeout){
+        return{
+            link: function (scope, element, attrs) {
+                $timeout(function(){
+                    element.html(scope.embeddedVideo);
+                    $compile(element.contents())(scope);
+                }, 0);
+            },
+            scope:{
+                embeddedVideo:'='
+            }
         }
     }
 ])
 .constant('resources.onlineSources', [
     {
-        title: 'google',
-        absUrl: 'http://google.com',
-        prettyUrl: 'google.com'
-    },
-    {
-        title: 'link 2',
-        absUrl: 'http://cnn.com',
-        prettyUrl: 'cnn.com'
+        title: 'eCW Users',
+        absUrl: 'ecwusers.com',
+        prettyUrl: 'ecwusers.com'
     }
 ])
 .constant('resources.videos', [
     {
-        title: 'youtube',
-        absUrl: 'http://youtube.com',
-        prettyUrl: 'youtube.com'
+        title: 'How to use Case Manager in eClinicalWorks',
+        absUrl: 'https://www.youtube.com/watch?v=EfYjtntvD_o',
+        prettyUrl: 'youtube.com',
+        embed: '<iframe width="560" height="315" src="https://www.youtube.com/embed/EfYjtntvD_o" frameborder="0" allowfullscreen></iframe>'
     }
-])
+]);
