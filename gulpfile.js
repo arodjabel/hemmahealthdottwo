7d9bc122ca64d8e810ed3e94d57f982589c0fe9a
arodjabel <arodjabel@gmail.com>
2017-01-02 14:28:42 -0600

    correcting some issues with cycle text; added some npm tasks for faster deploys

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,7 @@ var path = require('path');
 var Zip = require('adm-zip');
 var exec = require('exec');
 var filesToCompress,
-    dirsToCompress,
-    runTheArchive;
+    dirsToCompress;
 
 var archiver = require('archiver');
 var awsBeanstalk = require("node-aws-beanstalk");
@@ -16,137 +15,141 @@ filesToCompress = ['app.js', 'hemma.js', 'index.html', 'package.json'];
 dirsToCompress = ['images', 'server', 'site'];
 
 function createArchive(dir) {
-    return new Promise(function (resolve, reject) {
-        var archive,
-            output,
-            f,
-            d,
-            thisDir,
-            destPath,
-            finalizeCounter = 0,
-            zipName = './Archive.zip';
+  return new Promise(function (resolve, reject) {
+    var archive,
+        output,
+        f,
+        d,
+        thisDir,
+        destPath,
+        finalizeCounter = 0,
+        zipName = './Archive.zip';
 
-        // creates new in memory zip
+    // creates new in memory zip
 
-        output = fs.createWriteStream(__dirname + '/Archive.zip');
-        archive = archiver.create('zip', {});
-        archive.pipe(output);
+    output = fs.createWriteStream(__dirname + '/Archive.zip');
+    archive = archiver.create('zip', {});
+    archive.pipe(output);
 
-        // addLocalFile(String localPath, String zipPath)
-        // addLocalFolder(String localPath, String zipPath)
-        for (f = 0; f < filesToCompress.length; f++) {
-            // archive.addLocalFile(path.join(__dirname, filesToCompress[f]));
-            archive.file(path.join(__dirname, filesToCompress[f]), {name: filesToCompress[f]});
-            finalizeArchive();
-        }
+    // addLocalFile(String localPath, String zipPath)
+    // addLocalFolder(String localPath, String zipPath)
+    for (f = 0; f < filesToCompress.length; f++) {
+      // archive.addLocalFile(path.join(__dirname, filesToCompress[f]));
+      archive.file(path.join(__dirname, filesToCompress[f]), {name: filesToCompress[f]});
+      finalizeArchive();
+    }
 
-        for (d = 0; d < dirsToCompress.length; d++) {
-            // archive.addLocalFolder(path.join(__dirname, dirsToCompress[d]), path.join('./', dirsToCompress[d]));
-            archive.directory(path.join(__dirname, dirsToCompress[d]), path.join('./', dirsToCompress[d]), {name: dirsToCompress[d]});
-            finalizeArchive();
-        }
+    for (d = 0; d < dirsToCompress.length; d++) {
+      // archive.addLocalFolder(path.join(__dirname, dirsToCompress[d]), path.join('./', dirsToCompress[d]));
+      archive.directory(path.join(__dirname, dirsToCompress[d]), path.join('./', dirsToCompress[d]), {name: dirsToCompress[d]});
+      finalizeArchive();
+    }
 
-        function finalizeArchive() {
-            finalizeCounter++;
-            if (finalizeCounter === (filesToCompress.length + dirsToCompress.length)) {
-                // archive.writeZip(path.join(__dirname, zipName));
-                archive.finalize();
-                console.log('created blank Archive.zip');
-                resolve();
-            }
-        }
-    });
+    function finalizeArchive() {
+      finalizeCounter++;
+      if (finalizeCounter === (filesToCompress.length + dirsToCompress.length)) {
+        // archive.writeZip(path.join(__dirname, zipName));
+        archive.finalize();
+        console.log('created blank Archive.zip');
+        resolve();
+      }
+    }
+  });
 }
 
 function deleteArchive(dir, archive) {
-    return new Promise(function (resolve, reject) {
-        fs.unlink(archive, function (err) {
-            if (!err) {
-                console.log('deleted Archive.zip');
-                createArchive(dir).then(function () {
-                    resolve();
-                });
-            } else {
-                console.log('deleting Archive.zip failed');
-                resolve();
-            }
+  return new Promise(function (resolve, reject) {
+    fs.unlink(archive, function (err) {
+      if (!err) {
+        console.log('deleted Archive.zip');
+        createArchive(dir).then(function () {
+          resolve();
         });
+      } else {
+        console.log('deleting Archive.zip failed');
+        resolve();
+      }
     });
+  });
 }
 
-runTheArchive = function () {
-    return new Promise(function (resolve, reject) {
-        var dir,
-            archive;
+function runTheArchive() {
+  return new Promise(function (resolve, reject) {
+    var dir,
+        archive;
 
-        dir = path.resolve(__dirname);
-        archive = path.resolve(__dirname, 'Archive.zip');
+    dir = path.resolve(__dirname);
+    archive = path.resolve(__dirname, 'Archive.zip');
 
-        //does archive exist?
-        function startDeploy() {
-            return new Promise(function (resolve, reject) {
-                function closeTheFile(fd) {
-                    fs.close(fd);
-                    resolve();
-                }
-
-                fs.open(archive, 'r', function (err, fd) {
-                    if (err) {
-                        if (err.code === "ENOENT") {
-                            console.error('Archive.zip is not available');
-                            createArchive(dir).then(function () {
-                                closeTheFile(fd);
-                            });
-                        } else {
-                            deleteArchive(dir, archive).then(function () {
-                                closeTheFile(fd);
-                            });
-                        }
-                    } else {
-                        deleteArchive(dir, archive).then(function () {
-                            closeTheFile(fd);
-                        });
-                    }
-                });
-            });
+    //does archive exist?
+    function startDeploy() {
+      return new Promise(function (resolve, reject) {
+        function closeTheFile(fd) {
+          fs.close(fd);
+          resolve();
         }
 
-        startDeploy();
-        // startDeploy().then(deployToEb);
-    });
-};
+        fs.open(archive, 'r', function (err, fd) {
+          if (err) {
+            if (err.code === "ENOENT") {
+              console.error('Archive.zip is not available');
+              createArchive(dir).then(function () {
+                closeTheFile(fd);
+              });
+            } else {
+              deleteArchive(dir, archive).then(function () {
+                closeTheFile(fd);
+              });
+            }
+          } else {
+            deleteArchive(dir, archive).then(function () {
+              closeTheFile(fd);
+            });
+          }
+        });
+      });
+    }
+
+    startDeploy();
+    // startDeploy().then(deployToEb);
+  });
+}
 
 // i havent been able to get this one to work quite right yet
 function deployToEb() {
-    var spawn = require('child_process').spawn;
-    var ls = spawn('eb', ['deploy', '--staged']);
+  var spawn = require('child_process').spawn;
+  var ls = spawn('eb', ['deploy', '--staged']);
 
-    ls.stdout.setEncoding('utf8');
-    ls.stderr.setEncoding('utf8');
+  ls.stdout.setEncoding('utf8');
+  ls.stderr.setEncoding('utf8');
 
-    ls.stdout.on('data', function (data) {
+  ls.stdout.on('data', function (data) {
 
-        var str = data.toString(), lines = str.split(/(\r?\n)/g);
-        for (var i = 0; i < lines.length; i++) {
-            // Process the line, noting it might be incomplete.
-            if (lines[i])
-                console.log(lines[i]);
-        }
-    });
+    var str = data.toString(), lines = str.split(/(\r?\n)/g);
+    for (var i = 0; i < lines.length; i++) {
+      // Process the line, noting it might be incomplete.
+      if (lines[i])
+        console.log(lines[i]);
+    }
+  });
 
-    ls.stderr.on('data', function (data) {
-        var str = data.toString(), lines = str.split(/(\r?\n)/g);
-        for (var i = 0; i < lines.length; i++) {
-            // Process the line, noting it might be incomplete.
-            if (lines[i])
-                console.log(lines[i]);
-        }
-    });
+  ls.stderr.on('data', function (data) {
+    var str = data.toString(), lines = str.split(/(\r?\n)/g);
+    for (var i = 0; i < lines.length; i++) {
+      // Process the line, noting it might be incomplete.
+      if (lines[i])
+        console.log(lines[i]);
+    }
+  });
 
-    ls.on('close', function (code) {
-        console.log(code);
-    });
+  ls.on('close', function (code) {
+    console.log(code);
+  });
 }
+
+// function cacheTemplates(){
+//
+// }
 
 gulp.task('archive', runTheArchive);
 gulp.task('ebDeploy', deployToEb);
--- site/components/cycle-text/cycle-text.js
+++ site/components/cycle-text/cycle-text.js
@@ -1,113 +1,115 @@
 'use strict';
 
 angular.module('hemma.cycleText', [])
-    .directive('cycleText', ['$timeout', '$window', 'cycleText.shuffleArray', function ($timeout, $window, shuffleArray) {
+    .directive('cycleText', ['$timeout', '$window', 'cycleText.shuffleArray',
+      function ($timeout, $window, shuffleArray) {
         return {
-            bindToController: true,
-            controllerAs: 'cycleCtrl',
-            controller: ['$element', function (elem) {
-                var cycleCtrl = this,
-                    textSpot,
-                    wordList,
-                    randomOrder,
-                    s,
-                    timeoutLength,
-                    curWordState,
-                    loopTheWordsTimeout,
-                    loopTheLettersTimeout;
+          bindToController: true,
+          controllerAs: 'cycleCtrl',
+          controller: ['$element',
+            function (elem) {
+              var cycleCtrl = this,
+                  textSpot,
+                  wordList,
+                  randomOrder,
+                  s,
+                  timeoutLength,
+                  curWordState,
+                  loopTheWordsTimeout,
+                  loopTheLettersTimeout;
 
-                timeoutLength = 6000;
-                wordList = [
-                    'We are SuperUsers',
-                    'We do Revenue Cycle Management',
-                    'We optimize eCW',
-                    'We develop eCW SuperUsers',
-                    'We customize patient statements',
-                    'We do HIPAA compliance',
-                    'We do PCMH'
-                ];
+              timeoutLength = 6000;
+              wordList = [
+                'We are SuperUsers',
+                'We do Revenue Cycle Management',
+                'We optimize eCW',
+                'We develop eCW SuperUsers',
+                'We customize patient statements',
+                'We do HIPAA compliance',
+                'We do PCMH'
+              ];
 
-                textSpot = angular.element(elem[0].querySelectorAll('.cycle-text-text'));
+              textSpot = angular.element(elem[0].querySelectorAll('.cycle-text-text'));
 
-                function doShuffle() {
-                    textSpot.text(wordList[0]);
-                    randomOrder = shuffleArray(wordList);
-                    loopTheWords(randomOrder.length - 1);
-                }
+              function doShuffle() {
+                //clear everything
+                clearTimeout(loopTheLettersTimeout);
+                clearTimeout(loopTheWordsTimeout);
+                textSpot.text(wordList[0]);
+                randomOrder = shuffleArray(wordList);
+                loopTheWords(randomOrder.length - 1);
+              }
 
-                function loopTheWords(i) {
-
-                    loopTheWordsTimeout = setTimeout(function () {
-                        curWordState = '';
-                        textSpot.text('');
-                        putThisTextInThere(randomOrder[i]);
-                        if (--i >= 0) {
-                            loopTheWords(i);
-                        }
-                        else {
-                            doShuffle();
-                        }
-                    }, timeoutLength);
-                }
-
-                function loopTheLetters(i, timeout, word) {
-                    var theText = '';
-                    loopTheLettersTimeout = setTimeout(function () {
-                        curWordState = curWordState + word[i];
-                        textSpot.text(curWordState);
-                        if (--i >= 0) {
-                            loopTheLetters(i, timeout, word);
-                        }
-                        else {
-                            return;
-                        }
-                    }, timeout);
-                }
-
-                function putThisTextInThere(word) {
-                    var letterTimeoutLength;
-                    curWordState = '';
-                    textSpot.text('');
-
-                    letterTimeoutLength = 100;
-
-                    loopTheLetters(word.length - 1, letterTimeoutLength, word.split("").reverse().join(""));
-                }
-
-                function resetCycle() {
-                    clearTimeout(loopTheLettersTimeout);
-                    clearTimeout(loopTheWordsTimeout);
+              function loopTheWords(i) {
+                loopTheWordsTimeout = setTimeout(function () {
+                  curWordState = '';
                   textSpot.text('');
-                    textSpot.text(wordList[0]);
-                    curWordState = '';
-                }
+                  putThisTextInThere(randomOrder[i]);
+                  if (--i >= 0) {
+                    loopTheWords(i);
+                  } else {
+                    doShuffle();
+                  }
+                }, timeoutLength);
+              }
 
-                $window.onfocus = doShuffle;
-                $window.onblur = resetCycle;
+              function loopTheLetters(i, timeout, word) {
+                var theText = '';
+                loopTheLettersTimeout = setTimeout(function () {
+                  curWordState = curWordState + word[i];
+                  textSpot.text(curWordState);
+                  if (--i >= 0) {
+                    loopTheLetters(i, timeout, word);
+                  } else {
+                    return;
+                  }
+                }, timeout);
+              }
 
-                doShuffle();
+              function putThisTextInThere(word) {
+                var letterTimeoutLength;
+                curWordState = '';
+                textSpot.text('');
+                letterTimeoutLength = 100;
+                loopTheLetters(word.length - 1, letterTimeoutLength, word.split('').reverse().join(''));
+              }
 
-            }],
-            templateUrl: './site/components/cycle-text/cycle-text.html'
-        };
-    }])
-    .factory('cycleText.shuffleArray', [function () {
-        return function (array) {
-            var currentIndex = array.length, temporaryValue, randomIndex;
+              function resetCycle() {
+                clearTimeout(loopTheLettersTimeout);
+                clearTimeout(loopTheWordsTimeout);
+                textSpot.text('');
+                textSpot.text(wordList[0]);
+                curWordState = '';
+              }
 
-            // While there remain elements to shuffle...
-            while (0 !== currentIndex) {
+              $window.onfocus = doShuffle;
+              $window.onblur = resetCycle;
 
-                // Pick a remaining element...
-                randomIndex = Math.floor(Math.random() * currentIndex);
-                currentIndex -= 1;
+              doShuffle();
 
-                // And swap it with the current element.
-                temporaryValue = array[currentIndex];
-                array[currentIndex] = array[randomIndex];
-                array[randomIndex] = temporaryValue;
             }
-
-            return array;
+          ],
+          templateUrl: './site/components/cycle-text/cycle-text.html'
         };
+      }
+    ])
+    .factory('cycleText.shuffleArray', [function () {
+      return function (array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (currentIndex !== 0) {
+
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+      };
     }]);
